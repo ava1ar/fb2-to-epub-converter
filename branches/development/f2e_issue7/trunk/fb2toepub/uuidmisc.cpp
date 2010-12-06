252a73a807a1c85c08a344efdf4533848d7092f8
alex_bbkv@yahoo.com <alex_bbkv@yahoo.com@c07e1e63-38df-0198-a48b-ed168cd222d2>
2010-12-06 04:10:30 +0000

    some uuid stuff; code organisation;

--- branches/development/f2e_issue7/trunk/fb2toepub/uuidmisc.cpp
+++ branches/development/f2e_issue7/trunk/fb2toepub/uuidmisc.cpp
@@ -21,29 +21,81 @@
 #include "hdr.h"
 
 #include "uuidmisc.h"
+//#include <algorithm>
+#include <ctype.h>
 
 namespace Fb2ToEpub
 {
 
-    bool IsValidUUID(const String &id)
-    {
-        return false;
-    }
+//-----------------------------------------------------------------------
+bool IsValidUUID(const String &id)
+{
+    return (id.length() == 36 &&
+            isxdigit(id[0]) && isxdigit(id[1]) && isxdigit(id[2]) && isxdigit(id[3]) &&
+            isxdigit(id[4]) && isxdigit(id[5]) && isxdigit(id[6]) && isxdigit(id[7]) &&
+            id[8] == '-' &&
+            isxdigit(id[9]) && isxdigit(id[10]) && isxdigit(id[11]) && isxdigit(id[12]) &&
+            id[13] == '-' &&
+            isxdigit(id[14]) && isxdigit(id[15]) && isxdigit(id[16]) && isxdigit(id[17]) &&
+            id[18] == '-' &&
+            isxdigit(id[19]) && isxdigit(id[20]) && isxdigit(id[21]) && isxdigit(id[22]) &&
+            id[23] == '-' &&
+            isxdigit(id[24]) && isxdigit(id[25]) && isxdigit(id[26]) && isxdigit(id[27]) &&
+            isxdigit(id[28]) && isxdigit(id[29]) && isxdigit(id[30]) && isxdigit(id[31]) &&
+            isxdigit(id[32]) && isxdigit(id[33]) && isxdigit(id[34]) && isxdigit(id[35]));
+}
 
-    String GenerateUUID()
-    {
-        return "49fdf150-b8dd-11de-92bf-00a0d1e7a3b4";
-    }
+//-----------------------------------------------------------------------
+String GenerateUUID()
+{
+    return "49fdf150-b8dd-11de-92bf-00a0d1e7a3b4";
+}
     
-    void MakeAdobeKey(const String &id, char *adobeKey)
+//-----------------------------------------------------------------------
+void MakeAdobeKey(const String &uuid, char *adobeKey)
+{
+#if defined(_DEBUG)
+    if(!IsValidUUID(uuid))
+        Error("uuid internal error");
+#endif
+
+    const char *p = uuid.c_str();
+    bool high = true;
+    for(;;)
     {
-        char *p = adobeKey;
-        for(int i = 0; i < 1024/16; ++i)
+        char nibble;
+        char c = *p++;
+        switch(c)
         {
-            memcpy(p, "\x49\xfd\xf1\x50\xb8\xdd\x11\xde\x92\xbf\x00\xa0\xd1\xe7\xa3\xb4", 16);
-            p += 16;
+        case '\0':  return;
+        case '-':   continue;
+
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+            nibble = (c - '0');
+            break;
+
+        case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
+            nibble = (10 + (c - 'a'));
+            break;
+
+        case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
+            nibble = (10 + (c - 'A'));
+            break;
+        }
+
+        if(high)
+        {
+            high = false;
+            *adobeKey = (nibble << 4);
+        }
+        else
+        {
+            high = true;
+            *adobeKey++ |= nibble;
         }
     }
+}
 
 
 };  //namespace Fb2ToEpub
